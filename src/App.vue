24b8c0f4baa4fe423dad8862c286daa855d63147
RalucaNicola <rnicola@esri.com>
2019-12-20 01:52:18 +0100

    Add Footer, Guide and Menu components

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <section>
-      <b-button @click="clickMe">Say hello</b-button>
-    </section>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="tile is-ancestor is-gapless">
+    <div class="tile menu-width is-gapless">
+      <Menu></Menu>
+    </div>
+    <div class="tile is-vertical is-gapless">
+      <div class="map-container">
+        <div class="is-6 guide">
+          <Guide></Guide>
+        </div>
+        <div class="is-6 map">
+          <p>this is the map</p>
+        </div>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Guide from "./components/Guide.vue";
+import Footer from "./components/Footer.vue";
+import Menu from "./components/Menu.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld
-  },
-  methods: {
-    clickMe: function() {
-      alert("Hello!");
-    }
+    Guide,
+    Footer,
+    Menu
   }
 };
 </script>
 
 <style lang="scss">
+html,
+body,
 #app {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  overflow-y: hidden !important;
+}
+
+.menu-width {
+  max-width: 100px;
+}
+
+.guide,
+.map {
+  flex: 0.5;
+  height: 100%;
+  overflow: auto;
+  padding: 0.7em;
+}
+
+.map-container {
+  min-height: 0;
+  display: flex;
+  flex: 1;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="footer">
+    <p>
+      Map Projector has been developed for the
+      <a
+        href="http://www.gitta.info/website/en/html/index.html"
+        target="_blank"
+      >
+        GITTA learning platform
+      </a>
+      <br />
+      © Magnus Heitzler, Institute of Cartography and Geoinformation, ETH Zurich
+      (2019) license:
+      <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">
+        GPL v3
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style>
+.footer {
+  max-height: 50px;
+  font-size: 0.7em;
+  text-align: right;
+}
+</style>
