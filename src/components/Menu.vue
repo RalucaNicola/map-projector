6c75ca083c4a1168f72873f6a8ec9d5bf688b64a
RalucaNicola <rnicola@esri.com>
2019-12-21 20:31:23 +0100

    Change menu elements to buttons

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,41 +1,60 @@
 <template>
-  <div class="menu">
+  <div class="menu-element">
     <h1 class="title">Map Projector</h1>
-    <div class="menu-option">Introduction</div>
-    <div class="menu-option">Projection Surfaces</div>
-    <div class="menu-option">Map Distortions</div>
-    <div class="menu-option">Map Projections</div>
+    <MenuButton
+      v-for="step in menuSteps"
+      :key="step.type"
+      :text="step.text"
+      :onClick="setGuide(step.type)"
+    ></MenuButton>
   </div>
 </template>
 
+<script>
+import MenuButton from "./MenuButton.vue";
+export default {
+  name: "menu",
+  components: {
+    MenuButton
+  },
+  data: function() {
+    return {
+      menuSteps: [
+        {
+          text: "Introduction",
+          type: "intro"
+        },
+        {
+          text: "Projection Surfaces",
+          type: "surfaces"
+        },
+        {
+          text: "Map Distortions",
+          type: "distortions"
+        },
+        {
+          text: "Map Projections",
+          type: "projections"
+        }
+      ]
+    };
+  },
+  methods: {
+    setGuide: function(type) {
+      return function() {
+        console.log(type);
+      };
+    }
+  }
+};
+</script>
+
 <style scoped>
-.menu {
+.menu-element {
   background-color: #222831;
   padding: 0.3em;
   color: white;
   font-weight: bold;
   text-align: center;
-}
-
-.menu-option {
-  padding: 0;
-  cursor: pointer;
-  font-size: 0.8em;
-}
-
-.menu-option:hover {
-  color: #bbb;
-}
-
-.menu-option:first-of-type {
-  padding-top: 4em;
-}
-
-.menu-option:not(:last-child)::after {
-  content: "";
-  display: block;
-  width: 50px;
-  margin: 2.2em auto;
-  border-bottom: 1px solid white;
 }
 </style>
---
+++ src/components/MenuButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button @click="onClick">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "menu-button",
+  props: ["text", "onClick"]
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0;
+  cursor: pointer;
+  font-size: 0.8em;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  background-color: transparent;
+  border: none;
+}
+
+button:hover {
+  color: #00adb5;
+}
+
+button:first-of-type {
+  padding-top: 4em;
+}
+
+button:not(:last-child)::after {
+  content: "";
+  display: block;
+  width: 50px;
+  margin: 2.2em auto;
+  border-bottom: 1px solid white;
+}
+</style>
